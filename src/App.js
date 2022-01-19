63a9e5ff2feaee0ff5930fa4074b7aff5b10a606
kirryya <kir1987@inbox.ru>
2022-01-19 12:35:17 +0300

    19/01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+function hello() {
+    return (
+        alert('Hello IT_KAMASUTRA')
+    )
+}
+
+//hello();
+
+
+// function declaration
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering')
+    // полезное что-то
+
+    // обязана вернуть JSX
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log('AppTitle rendering')
+    return <>This is APP component</>
+}
+
+function Rating() {
+    console.log('Rating rendering')
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion() {
+    console.log('Accordion rendering')
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function Star() {
+    console.log('Star rendering')
+    return (
+        <div>star</div>
+    )
+
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering')
+    return (
+        <div>
+            <h3>Меню</h3>
+        </div>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+
+    )
+
 }
 
 export default App;
