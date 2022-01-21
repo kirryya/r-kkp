64ac7f1912e4d3e97a2f545309e834a96350568d
kirryya <kir1987@inbox.ru>
2022-01-21 10:55:19 +0300

    21/01

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,26 @@ function App() {
     // обязана вернуть JSX
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={'This is APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title="Menu1"/>
+            <Accordion title="Menu2"/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    console.log('AppTitle rendering')
-    return <>This is APP component</>
+function PageTitle(props:any) {
+    console.log('PageTitle rendering')
+    return <h1>{props.title}</h1>
 }
 
 
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,23 +1,78 @@
-
-function Rating() {
+function Rating(props:any) {
     console.log('Rating rendering')
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
+    if (props.value === 1){
+        return (
+            <div>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+        )
+    }
+    if (props.value === 2){
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 3){
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 4){
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 5){
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        )
+    }
+        return (
+            <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
         </div>
     )
 }
 
-function Star() {
+function Star(props: any) {
     console.log('Star rendering')
-    return (
-        <div>star</div>
-    )
-
+    if (props.selected === true) {
+        return <span><b>star</b></span>
+    } else {
+        return <span>star</span>
+    }
 }
 
 export default Rating;
