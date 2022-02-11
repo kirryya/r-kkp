a5c948d652869ec64a07af83eb2b14fb7ee5cd5f
kirryya <kir1987@inbox.ru>
2022-02-11 20:15:44 +0300

    Conditional Rendering

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -7,20 +7,12 @@ type AccordionPropsType = {
 
 export function Accordion(props: AccordionPropsType) {
     console.log('Accordion rendering')
-    if (props.collapsed) {
-        return (
-            <div>
-                <AccordionTitle title={props.titleValue}/>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <AccordionTitle title={props.titleValue}/>
-                <AccordionBody/>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            {!props.collapsed && <AccordionBody/>}
+        </div>
+    )
 }
 
 type AccordionTitlePropsType = {
